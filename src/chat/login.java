468a52f06f4934333805526f7053847284d7607b
ezzeddine <ezzeddine@gmail.com>
2018-01-10 01:32:57 +0100

    récupération les données formulaire entrez par l'utilisateur et le stock das la clase 'user'

--- src/chat/login.java
+++ src/chat/login.java
@@ -1,41 +1,65 @@
 package chat;
 
+
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class login
- */
+
+import testcnx.user;
+
+
+
 @WebServlet("/login")
 public class login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	static PrintWriter out;
+	 
+	public login() {
+		super();
+
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
 	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		out = response.getWriter();
+		 
+		String email=request.getParameter("email2");
+		String psw=request.getParameter("psw2");
+	    String t1=""+email;
+	    String t2=""+psw;
+	    
+	  	       
+	    user User = new user();
+	    if(t1.equals("") || t2.equals("")) {
+			User.setFirstname(request.getParameter("firstname"));
+			User.setLastname(request.getParameter("lastname"));
+			User.setEmail(request.getParameter("email"));
+			User.setPsw(request.getParameter("psw")); 
+ 		    
+	    
+	    }
+	   
+	    
+	  	       
+	  
+				
+				
+	
+}
+	
 }
