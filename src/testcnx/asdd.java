5befe036519762324970659fe6b73f3756d68c8a
ezzeddine <ezzeddine@gmail.com>
2018-01-13 17:13:43 +0100

    creation du nouvelle class 'add' qui doit contient les fonctions des manipulation de donner.  et une fonction appeller 'adduser' qui peut insert un utilisateur aux basse de donne

---
+++ src/testcnx/asdd.java
@@ -0,0 +1,39 @@
+package testcnx;
+
+import java.beans.Statement;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+
+public class asdd {
+	static PreparedStatement prt;
+	static Statement st = null;
+	static int rs;
+
+	public static void adduser(user Users) throws SQLException, ClassNotFoundException, ServletException, IOException {
+
+		String sql = "INSERT INTO `utilisateur` (`firstname`, `lastname`, `email`, `psw`) VALUES (?,?,?,?)";
+		if (Users.getLastname().equals("") || Users.getFirstname().equals("") || Users.getEmail().equals("")
+				|| Users.getPsw().equals("")) {
+
+		} else
+			try {
+
+				Testcnx.conn();
+				prt = Testcnx.conn.prepareStatement(sql);
+				prt.setString(1, Users.getFirstname());
+				prt.setString(2, Users.getLastname());
+				prt.setString(3, Users.getEmail());
+				prt.setString(4, Users.getPsw());
+				rs = prt.executeUpdate();
+				Testcnx.conn.close();
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
+
+	}
+
+}
